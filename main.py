36fd84fb7b31fb627ac5f9b6c50db0e272cca13b
Anselmo <anselmo.cstecnologia@gmail.com>
2026-02-02 17:02:43 -0300

    utilizando cadeias no langchain

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
-
+from langchain_core.output_parsers import StrOutputParser
 
 import os
 
@@ -11,26 +11,27 @@ load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 #cliente = OpenAI(api_key=api_key)
 
-numeros_dias = 7
-destino = "Gramado, Brasil"
-numero_criancas = 1
 
 
 modelo_prompt = PromptTemplate(
-    template="Crie um itinerário detalhado para uma viagem de {dias} dias para {destino}, incluindo atividades adequadas para {criancas} criança(s)."
+    template="Sugira um filme dado meu {interesse}.",
+    input_variables=["interesse"]
 )
 
-prompt = modelo_prompt.format(
-    dias=numeros_dias,
-    destino=destino,
-    criancas=numero_criancas
-)
 
 
 modelo = ChatOpenAI(
     model="gpt-4.1-mini",
     temperature=0.5
-git
-    
-resposta = modelo.invoke(prompt)
-print(resposta.content)
+)
+
+cadeia = modelo_prompt | modelo | StrOutputParser()
+
+resposta = cadeia.invoke(
+    {
+        "interesse" : " ficção cientifica"
+    }
+)
+
+
+print(resposta)

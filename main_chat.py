f4f7a099b7779ca3964be767aae0fcc26f0ff409
Anselmo <anselmo.cstecnologia@gmail.com>
2026-02-03 16:38:41 -0300

    chat langchain

--- main_chat.py
+++ main_chat.py
@@ -1,36 +1,59 @@
-
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-import os
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
-
+# Carrega variáveis de ambiente (.env)
 load_dotenv()
 
-api_key = os.getenv("OPENAI_API_KEY")
-
+# Modelo de linguagem
 modelo = ChatOpenAI(
     model="gpt-4.1-mini",
     temperature=0.5
 )
 
-prompt_sugestao = ChatPromptTemplate(
-    [
-        ('sytem', 'Você é um assistente de viagem que responde perguntas sobre destinos turísticos, seu nome é Sr.Viagens'),
-        ('placeholder', '{historico}'),
-        ('human', '{query}'),
-    ]
-)
+# Prompt correto usando from_messages
+prompt_sugestao = ChatPromptTemplate.from_messages([
+    # system estava escrito errado (sytem)
+    ("system", "Você é um assistente de viagem que responde perguntas sobre destinos turísticos. Seu nome é Sr. Viagens."),
+    ("placeholder", "{historico}"),
+    # Mensagem do usuário
+    ("human", "{query}")
+])
 
+# Cadeia: Prompt → Modelo → Parser
 cadeia = prompt_sugestao | modelo | StrOutputParser()
 
+memoria =  {}
+sessao = 'lanchain'
+
+def historioco_de_sessao(sessao : str):
+    if sessao not in memoria:
+        memoria[sessao] = InMemoryChatMessageHistory()
+    return memoria[sessao]
+
+
+# Perguntas
 lista_perguntas = [
     "Qual a capital da Taiwan?",
     "Qual é a melhor época do ano para visitar?"
 ]
 
-for pergunta in lista_perguntas:
-    resposta = modelo.invoke(pergunta)
-    print(f' pergunta : {pergunta} \n resposta : {resposta.content}')
+cadeia_com_memoria = RunnableWithMessageHistory(
+    runnable=cadeia,
+    get_session_history=historioco_de_sessao,
+    input_message_key="query",
+    history_message_key="historico"
+)
 
+# Execução correta
+for pergunta in lista_perguntas:
+    resposta = cadeia_com_memoria.invoke(
+        {"query": pergunta}, # invoke precisa de dicionário
+        config={"session_id": sessao}
+    )
+    
+    print(f"Pergunta: {pergunta}")
+    print(f"Resposta: {resposta}\n")  # resposta já é string
